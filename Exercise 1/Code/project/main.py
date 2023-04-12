52949f0354f445af53521f9f900cbb7297e7d8b9
Zisis Sourlas <sourlas.zisis@upnet.gr>
2023-04-12 20:49:40 +0300

    Updates to the code

--- Exercise 1/Code/project/main.py
+++ Exercise 1/Code/project/main.py
@@ -6,23 +6,24 @@ import random
 
 #Reading the dataset
 
-data_raw  = pd.read_csv('./dataset.csv', delimiter=';', low_memory=False)
+data_raw  = pd.read_csv('./dataset.csv', delimiter=';', low_memory=False,encoding="utf-8-sig")
 
-#Data preprocessing
+data_processed = data_raw
+######## Data preprocessing ##########
 
 #Turning categorical values into numerical ones
 
 #Itterate over all the columns containing categorical values
 
-for column in data_raw['user','gender','class']:
-    columnObj = data_raw[column]
+for column in data_processed[['user','gender','class']]:
+    columnObj = data_processed[column]
 
     layer = keras.layers.StringLookup() #Creating string covnersion layer
     layer.adapt(columnObj.values) #Adapting layer to column data (making the vocabulary)
     x = layer(columnObj.values) #Getting data through the layer
 
-    data_raw[column] = np.array(x) #Replacing values int the original dataset
+    data_processed[column] = np.array(x) #Replacing values in the original dataset
 
 
 print('Final: ')
-print(data_raw)
+print(data_processed)
